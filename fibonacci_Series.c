99b43ad1b14041e9c35a29dbb23aa134fc9b8eee
Samiksha-mane <samikshamane2005@gmail.com>
2025-04-05 20:31:38 +0530

    Add files via upload

---
+++ fibonacci_Series.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() 
+{
+    int n, first = 0, second = 1, next;
+
+    printf("Enter the number of terms: ");
+    scanf("%d", &n);
+
+    printf("Fibonacci Series: ");
+	int i;
+    for (i = 1; i <= n; ++i) 
+	{
+        printf("%d ", first);
+        next = first + second;
+        first = second;
+        second = next;
+    }
+
+    printf("\n");
+    return 0;
+}
+
