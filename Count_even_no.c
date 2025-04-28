4130e14042f52350fcf3533cd9e277b34e762ee9
Samiksha-mane <samikshamane2005@gmail.com>
2025-04-28 21:35:18 +0530

    Add files via upload

---
+++ Count_even_no.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main() 
+{
+    int n, i, count = 0;
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) 
+	{
+        scanf("%d", &arr[i]);
+    }
+    for (i = 0; i < n; i++) 
+	{
+        if (arr[i] % 2 == 0) 
+		{
+            count++;
+        }
+    }
+    printf("Number of even elements: %d\n", count);
+    return 0;
+}
+
